12e9befa2265e7ce59a7d2a377b12749bb26bae5
DehaoDong <ddong@tcd.ie>
2023-12-02 19:21:28 +0000

    backend recovery

--- backend/ASE_Group10/settings.py
+++ backend/ASE_Group10/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-# from decouple import Config, RepositoryEnv
+from decouple import Config, RepositoryEnv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
