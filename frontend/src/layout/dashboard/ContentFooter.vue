e5a7a690dc4791eff6990ec62aef622df785afae
Kaiyu Chen <i@iocky.com>
2023-12-02 06:51:29 +0000

    update: pagefoot

--- frontend/src/layout/dashboard/ContentFooter.vue
+++ frontend/src/layout/dashboard/ContentFooter.vue
@@ -11,16 +11,14 @@
       <div class="copyright d-flex flex-wrap">
         &copy; Coded with
         <i class="fa fa-heart heart"></i> by
-        <a
-          href="https://github.com/Group10-ResponseSystemToCityDisaster"
-          target="_blank"
-        >
+        <a href="https://github.com/ase-g10/rscd" target="_blank">
           &nbsp; Group 10.</a
         >&nbsp; Designed by
-        <a
-          href="https://github.com/Group10-ResponseSystemToCityDisaster"
-          target="_blank"
+        <a href="https://github.com/ase-g10/rscd" target="_blank"
           >&nbsp; Group 10.</a
+        >&nbsp; Check out the
+        <a href="https://upup.iocky.com/s/rscd/" target="_blank"
+          >&nbsp; Service Status.</a
         >
       </div>
     </div>
