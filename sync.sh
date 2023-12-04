9a04aaeb79d0b90f78d9fe6f41b71a3d93e19458
Kaiyu Chen <i@iocky.com>
2023-12-04 16:02:10 +0000

    update: branch sync .sh

---
+++ sync.sh
@@ -0,0 +1,17 @@
+git checkout main
+git pull origin main
+
+git checkout back-dev
+git pull origin main
+git merge main
+git push origin back-dev
+
+git checkout workflow
+git pull origin main
+git merge main
+git push origin workflow
+
+git checkout front-dev
+git pull origin main
+git merge main
+git push origin front-dev
