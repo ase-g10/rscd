8a29896da1cb35f221f7f6b0f12d866f5d37f8b3
Kaiyu Chen <i@iocky.com>
2024-02-06 15:44:26 +0000

    update: try to figure the staging api for test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,21 @@ module.exports = defineConfig({
   
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // 使用环境变量来设置不同的baseUrl
+      let baseUrl;
+
+      // 根据GitHub Actions中设置的环境变量来动态决定baseUrl
+      if (process.env.CI_ENVIRONMENT_NAME === 'staging') {
+        // Staging环境的URL
+        baseUrl = 'https://rscdapistaging.iocky.com';
+      } else if (process.env.CI_ENVIRONMENT_NAME === 'production') {
+        // 生产环境的URL
+        baseUrl = 'https://rscdapi.iocky.com';
+      } else {
+        baseUrl = 'http://localhost:8080';
+      }
+      config.baseUrl = baseUrl;
+      return config;
     },
     "baseUrl": "http://localhost:8080",
   },
