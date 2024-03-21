15858c53e876751b1404beba5b38616d1702ac55
amoslue <3437615293@qq.com>
2024-03-21 15:32:11 +0000

    fix register issues. add role identify TODO.

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -4,35 +4,20 @@ import axios from 'axios';
 
 export function userLogin(data) {
   return service({
-    url: "um/login/log_in/",
-    method: "post",
+    url: "/um/auth/login/",
+    method: "POST",
     data,
   })
 }
 
-//TODO: 后端补全注册信息入库，此处仅为示例代码
-// 模拟一个成功的注册响应
-const mockResponse = {
-  data: {
-    user: {
-      id: 1,
-      email: "example@example.com",
-      name: "gg",
-    },
-    message: "Registration successful",
-  },
-};
-
 export const userRegister = async (userData) => {
-  const response = await fetch('/register', {
+  const response = await service({
+    url:"/um/auth/register/",
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
+    data: userData,
   });
-  if (!response.ok) {
+  if (response.status !== 201) {
     throw new Error('Failed to register');
   }
-  return response.json();
+  return response.data;
 };
