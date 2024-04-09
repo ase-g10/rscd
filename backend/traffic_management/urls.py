9b5cbe7a0b051258f1c42570ad776e78df112ddf
Jiawei Shen <910650692@qq.com>
2024-04-09 15:18:19 +0100

    add:get/save drivingLocation function

--- backend/traffic_management/urls.py
+++ backend/traffic_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from . import views
 router = SimpleRouter()
-
+router.register(r'trafficview', views.TrafficView, basename='TrafficView')
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- backend/traffic_management/views.py
+++ backend/traffic_management/views.py
@@ -1,3 +1,24 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from models.models import DrivingLocation
 
-# Create your views here.
+
+class TrafficView(viewsets.ViewSet):
+    @action(detail=False, methods=['get'])
+    def get_driving_location(self, request):
+        locations = DrivingLocation.objects.filter(is_disabled=False)
+        locations_list = [{"username": loc.username, "latitude": loc.latitude, "longitude": loc.longitude} for loc in locations]
+        return JsonResponse({"status": "success","locations": locations_list})
+
+
+    @action(detail=False, methods=['post'])
+    def save_driving_location(self, request):
+        username = request.data.get("username")
+        latitude = request.data.get("latitude")
+        longitude = request.data.get("longitude")
+        if username is None or latitude is None or longitude is None:
+            return JsonResponse({"status": "error", "message": "Invalid request."})
+        DrivingLocation.objects.create()
+        return JsonResponse({"status": "success", "message": "Location saved successfully."})
